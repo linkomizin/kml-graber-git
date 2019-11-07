57c0115a35ceb9180b4e4ad4ae027f606d4c7166
linkomizin <linkomizin@gmail.com>
2019-11-07 23:05:06 +0300

    жжш

--- test-1.py
+++ test-1.py
@@ -10,28 +10,11 @@ def name_bs(element):
         name_bs(feature)
 
 
-if __name__ == '__main__':
-
-    fname = "gps.xml"
-
-    with open(fname,"rb") as kmlFile:
-        k = kml.KML()
-        file = kmlFile.read()
-        k.from_string(file)
-        features =list(k.features())
-        f2 = list(features[0].features())
-        count_all = len(f2) - 1
-        count = 0
-        while count <= count_all:
-            count
-            count = count - 1
-            # coordinat = ((str(f2[count].geometry))[6:]).replace('(','').replace(')','').replace(' ',',')
-            # latitude, longitude = coordinat.split(',', 1)
-            s = (f2[count].name)
-            
-            
-
-
+    
+        # if a == (len(f2) - 1):
+        #     break
+def signal_y():
+    
     for yes_signal in s:
         try:
             if s.find('dBm'):
@@ -42,9 +25,10 @@ if __name__ == '__main__':
                 yes_signal = ('-' + signal)
             else:
                 continue
-            print(yes_signal)
+            # print(yes_signal)
         except ValueError:
             pass    
+def signal_n():
     for no_signal in s:
         try:
             if s.find('БС 0'):
@@ -53,10 +37,70 @@ if __name__ == '__main__':
             else:
                 continue
                 
-            print (no_signal)
+            # print (no_signal)
         
         except ValueError:
             pass
+        
+        
+
+if __name__ == '__main__':
+
+    fname = "gps.xml"
+
+    with open(fname,"rb") as kmlFile:
+        k = kml.KML()
+        file = kmlFile.read()
+        k.from_string(file)
+        features =list(k.features())
+        f2 = list(features[0].features())
+
+    for s in f2:
+        a = 0
+        while a < len(f2):
+            a = a + 1
+            s = f2[a].name
+            # print(s)
+            try:
+                if s.find('dBm'):
+                
+                    signal = (s.replace('dBm', ''))
+                
+                    bs_signal, signal = (signal.split('-', (1)))
+                    yes_signal = ('-' + signal)
+                else:
+                    continue
+                # print(a," : --- " ,yes_signal)
+            except ValueError:
+                pass
+
+            for yes_signal in s:       
+                try:
+                    if s.find('БС 0'):
+                    
+                        no_signal = ['0']
+                        print (a," : --- " ,no_signal)
+                    else:
+                        continue
+                
+                
+        
+                except ValueError:
+                    pass    
+        
+        # print (s)
+        # count_all = len(f2) - 1
+        # count = 0
+        # while count <= count_all:
+        #     count
+        #     count = count - 1
+        #     # coordinat = ((str(f2[count].geometry))[6:]).replace('(','').replace(')','').replace(' ',',')
+        #     # latitude, longitude = coordinat.split(',', 1)
+        #     s = (f2[count].name)
+            
+            
+
+
 
                     # print(len(yes_signal))
 
