fca2b6d53a390c941d002c2d98aff7f02aaabee6
linkomizin <linkomizin@gmail.com>
2020-02-20 17:24:06 +0300

    add progress bar

--- test_qt5.py
+++ test_qt5.py
@@ -37,6 +37,12 @@ class MyWindow(QtWidgets.QWidget):
         self.vbox.addWidget(self.button3)
         self.button3.clicked.connect(self.open_file)
 
+        self.progressBar = QtWidgets.QProgressBar(self)
+        # self.progressBar.adjustSize()
+        self.vbox.addWidget(self.progressBar)
+        #  self.progressBar.setAlignment(QtCore.Qt.AlignHCenter)
+
+
 
     def open_dir(self, parent):
         self.dirName = QtWidgets.QFileDialog.getExistingDirectory(parent=window, directory=QtCore.QDir.currentPath())
@@ -53,7 +59,10 @@ class MyWindow(QtWidgets.QWidget):
         self.fileName = self.f[0]
         self.label2.setText(self.fileName)
         # return self.fileName
-        read_kml(self.fileName)
+        self.read_kml(self.fileName)
+
+        # self.progressBar = QtWidgets.QProgressBar(self)
+        # self.progressBar.setGeometry(200, 80, 250, 20)
 
 
 
@@ -63,67 +72,68 @@ class MyWindow(QtWidgets.QWidget):
         #button = QtWidgets.QPushButton("Нажми меня")
         #self.button.setDisabled(True)
 
-def read_kml(fileName):
-    print(fileName)
-    with open(fileName, 'rb') as kmlFile:
-        k = kml.KML()
-        file = kmlFile.read()
-        k.from_string(file)
-        features =list(k.features())
-        f2 = list(features[0].features())
-        a = 0
+    def read_kml(self, fileName):
+        print(fileName)
+        with open(fileName, 'rb') as kmlFile:
+            k = kml.KML()
+            file = kmlFile.read()
+            k.from_string(file)
+            features =list(k.features())
+            f2 = list(features[0].features())
+            a = 0
 
-        sorce_all = []
-        all_signal = []
-        num_point = []
-        coordinat_all = []
-        latitude_all = []
-        longitude_all = []
-        signal_all = []
+            sorce_all = []
+            all_signal = []
+            num_point = []
+            coordinat_all = []
+            latitude_all = []
+            longitude_all = []
+            signal_all = []
 
-        while a < len(f2) - 1:
-            a = a + 1
-            s = f2[a].name
+            while a < len(f2) - 1:
+                a = a + 1
 
+                s = f2[a].name
+                num_point.append(a)
+                self.progressBar.setValue(a)
 
-            num_point.append(a)
-            if "dBm" in s:
+                if "dBm" in s:
 
-                signal = (s.replace('dBm',''))
+                    signal = (s.replace('dBm',''))
 
-                de, signal = (signal.split("-",(1)))
-                de = 0
-                signal_y = ('-' + signal)
-                signal_all.append(signal_y)
-
-
-            else :
-                if 'БС 0' in s:
-
-                    signal_n = ('0')
-
-                    signal_all.append(signal_n)
+                    de, signal = (signal.split("-",(1)))
+                    de = 0
+                    signal_y = ('-' + signal)
+                    signal_all.append(signal_y)
 
 
-            coordinat = ((str(f2[a].geometry))[6:]).replace('(', '').replace(')', '').replace(' ', ',')
-            longitude, latitude = coordinat.split(',', 1)
+                else :
+                    if 'БС 0' in s:
 
-            latitude_all.append(latitude)
-            longitude_all.append(longitude)
+                        signal_n = ('0')
 
-            point =[num_point, signal_all, latitude_all, longitude_all]
+                        signal_all.append(signal_n)
 
-        allio = {"num_point": num_point, "signal_all": signal_all, "latitude_all": latitude_all, "longitude_all": longitude_all}
-        # pprint(allio)
-        with open('mifile.txt', 'w') as mi:
-            mi.write(str(allio))
 
-        df = pd.DataFrame(allio, index=[num_point])
+                coordinat = ((str(f2[a].geometry))[6:]).replace('(', '').replace(')', '').replace(' ', ',')
+                longitude, latitude = coordinat.split(',', 1)
 
-        fig = go.Figure(go.Densitymapbox(lat=df.latitude_all, lon=df.longitude_all, z=df.signal_all, radius=10))
-        fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
-        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-        fig.show()
+                latitude_all.append(latitude)
+                longitude_all.append(longitude)
+
+                point =[num_point, signal_all, latitude_all, longitude_all]
+
+            allio = {"num_point": num_point, "signal_all": signal_all, "latitude_all": latitude_all, "longitude_all": longitude_all}
+            # pprint(allio)
+            with open('mifile.txt', 'w') as mi:
+                mi.write(str(allio))
+
+            df = pd.DataFrame(allio, index=[num_point])
+
+            fig = go.Figure(go.Densitymapbox(lat=df.latitude_all, lon=df.longitude_all, z=df.signal_all, radius=10))
+            fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
+            fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+            fig.show()
 
 
 
